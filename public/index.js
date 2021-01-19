56b2ae970903e6c5040d73439d4f553f4050be6a
Sir Code <taylorschneider07@yahoo.com>
2021-01-18 21:40:33 -0500

    Fully redone login system

--- public/index.js
+++ public/index.js
@@ -1,4 +1,5 @@
-const accountModal = document.querySelector(".rtAccount");
+const $ = document.querySelector.bind(document);
+const accountModal = $(".rtAccount");
 let acntOpen = false;
 let currentUser;
 let currentUserDB;
@@ -6,25 +7,40 @@ let currentUserDB;
 accountModal.addEventListener("click", () => {
     acntOpen = !acntOpen;
     if (acntOpen) {
-        document.querySelector(".rtAccountModal").style.top = "100px";
-        document.querySelector(".rtAccountModal").style.right = "25px";
+        $(".rtAccountModal").style.top = "100px";
+        $(".rtAccountModal").style.right = "25px";
     } else {
-        document.querySelector(".rtAccountModal").style.top = "-100vh";
-        document.querySelector(".rtAccountModal").style.right = "-100vw";
+        $(".rtAccountModal").style.top = "-100vh";
+        $(".rtAccountModal").style.right = "-100vw";
     }
 });
 
-document.querySelector(".passwordInput").addEventListener("input", (e) => {
-    document.querySelector(".passwordInputText").value = e.target.value;
+$(".showSignUpForm").addEventListener("click", (e) => {
+    document.querySelectorAll("form").forEach((el) => {
+        el.style.display = "none";
+    });
+    $(".signUpForm").style.display = "unset";
 });
 
-document.querySelector(".signUpButton").addEventListener("click", () => {
-    const email = document.querySelector(".emailInput").value;
-    const password = document.querySelector(".passwordInput").value;
+$(".showLogInForm").addEventListener("click", (e) => {
+    document.querySelectorAll("form").forEach((el) => {
+        el.style.display = "none";
+    });
+    $(".logInForm").style.display = "unset";
+});
+
+$(".signUpWithForm").addEventListener("click", () => {
+    const username = $("#newUsernameInput").value;
+    const email = $("#newEmailInput").value;
+    const password = $("#newPasswordInput").value;
     auth.createUserWithEmailAndPassword(email, password).then((cred) => {
-        db.collection("users").add({
+        db.collection("users").doc(cred.user.uid).set({
             userId: cred.user.uid,
-            userRole: "basic"
+            userRole: "basic",
+            userName: username
+        });
+        db.collection("usernames").doc("claimed").update({
+            names: firebase.firestore.FieldValue.arrayUnion(username)
         });
         auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL).then(() => {
             auth.signInWithEmailAndPassword(email, password).then((cred) => {
@@ -34,9 +50,9 @@ document.querySelector(".signUpButton").addEventListener("click", () => {
     });
 });
 
-document.querySelector(".logInButton").addEventListener("click", () => {
-    const email = document.querySelector(".emailInput").value;
-    const password = document.querySelector(".passwordInput").value;
+$(".logInWithForm").addEventListener("click", () => {
+    const email = $("#emailInput").value;
+    const password = $("#passwordInput").value;
     auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL).then(() => {
         auth.signInWithEmailAndPassword(email, password).then((cred) => {
             currentUser = cred.user;
@@ -44,44 +60,49 @@ document.querySelector(".logInButton").addEventListener("click", () => {
     });
 });
 
-document.querySelector(".logOutButton").addEventListener("click", () => {
+$(".logOutOfProfile").addEventListener("click", () => {
     auth.signOut().then(() => {
         currentUser = undefined;
     });
 });
 
-document.querySelector(".updateProfileImgButton").addEventListener("click", () => {
+$(".updateProfilePicture").addEventListener("click", () => {
     auth.currentUser.updateProfile({
-        photoURL: document.querySelector(".profilePictureLink").value
+        photoURL: $("#newPictureInput").value
     });
     updateProfile();
 });
 
 auth.onAuthStateChanged((user) => {
     if (user) {
-        db.collection("users").get().then((snapshot) => {
-            currentUserDB = snapshot.docs;
+        db.collection("users").doc(user.uid).get().then((doc) => {
+            currentUserDB = doc.data();
+            currentUser = user;
+            updateProfile();
         });
-    }
-    if (user) {
-        currentUser = user;
-        updateProfile();
     } else resetProfile();
 });
 
 function updateProfile() {
-    document.querySelector(".rtProfile.signUp.curProfile").classList.remove("curProfile");
-    document.querySelector(".rtProfile.loggedIn").classList.add("curProfile");
-    document.querySelector(".rtProfileEmail").innerText = currentUser.email;
-    if (currentUser.photoURL) document.querySelector(".rtAccountImage").src = currentUser.photoURL;
-    if (currentUserDB) {
-        console.log(currentUserDB[currentUser.uid]);
-    }
+    try {
+        if (currentUser.photoURL) $(".rtAccountImage").src = currentUser.photoURL;
+        $(".profileEmail").innerText = currentUser.email;
+        if (currentUserDB) $(".profileUserName").innerText = currentUserDB.userName;
+        document.querySelectorAll("form").forEach((el) => {
+            el.style.display = "none";
+        });
+        $(".profileForm").style.display = "unset";
+    } catch (e) {}
 }
 
 function resetProfile() {
-    document.querySelector(".rtProfile.signUp").classList.add("curProfile");
-    document.querySelector(".rtProfile.loggedIn.curProfile").classList.remove("curProfile");
-    document.querySelector(".rtProfileEmail").innerText = currentUser.email;
-    if (currentUser.photoURL) document.querySelector(".rtAccountImage").src = 'https://mistersircode.com/defaultUser';
+    try {
+        $(".rtAccountImage").src = 'https://mistersircode.com/defaultUser';
+        $(".profileEmail").innerText = "You shouldnt be here. Refresh the page";
+        $(".profileUserName").innerText = "Not Signed In";
+        document.querySelectorAll("form").forEach((el) => {
+            el.style.display = "none";
+        });
+        $(".logInForm").style.display = "unset";
+    } catch (e) {}
 }
