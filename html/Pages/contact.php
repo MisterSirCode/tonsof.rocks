639ea4b9f5864aa086507c18cceb6f2b0ee56198
Sir Code <taylorschneider07@yahoo.com>
2021-01-27 14:22:56 -0500

    update main stable page

---
+++ html/Pages/contact.php
@@ -0,0 +1,96 @@
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="../Icons/ms-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="57x57" href="../Icons/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="../Icons/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="../Icons/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="../Icons/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="../Icons/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="../Icons/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="../Icons/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="../Icons/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="../Icons/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192" href="../Icons/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="../Icons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="../Icons/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="../Icons/favicon-16x16.png">
+
+	<!-- Primary Meta Tags -->
+	<title>SirCodes Homepage</title>
+	<meta name="title" content="Mister SirCodes Homepage">
+	<meta name="description" content="Main website for my Projects and Portfolio">
+	<meta name="theme-color" content="#ffffff">
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="Mister SirCodes Homepage">
+	<meta property="og:description" content="Main website for my Projects and Portfolio">
+	<meta property="og:image" content="https://mistersircode.com/Images/SCLOGOSmall.png">
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image">
+	<meta property="twitter:title" content="Mister SirCodes Homepage">
+	<meta property="twitter:description" content="Main website for my Projects and Portfolio">
+	<meta property="twitter:image" content="https://mistersircode.com/Images/SCLOGOSmall.png">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+		integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+	<style>
+
+	</style>
+	<link rel="stylesheet" href="../Stylesheets/index.css">
+</head>
+
+<body>
+	<nav class="navbar navbar-expand-md">
+		<a class="navbar-brand" href="/">
+			<img src="../Images/SCLOGOSmall.png" alt="" height="30px">
+		</a>
+		<button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="main-navigation">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link" href="../">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="./projects.php">Projects</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="./portfolio.php">Portfolio</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="">Contact</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+	<header class="page-header header container-fluid">
+		<div class="overlay"></div>
+		<div class="description">
+			<h1>Contact Page</h1>
+			<p>Contact info if youre curious about something, or wish to hire me</p>
+			<br>
+			<h6>Background Arts by <a href="https://www.pexels.com/@earano">Emiliano Arano</a></h6>
+		</div>
+	</header>
+	<footer class="page-footer">
+		<div class="footer-copyright text-center">Copyright © 2021</div>
+		<div class="footer-copyright text-center">Owned and Distributed by Taylor Schneider</div>
+	</footer>
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+	integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
+	integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous">
+</script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
+	integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous">
+</script>
+
+</html>
