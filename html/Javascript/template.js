633e57bc5ab165642e75c5c262df3edb3692afa2
Sir Code <44554976+MisterSirCode@users.noreply.github.com>
2021-05-19 11:16:32 -0400

    Create template.js

---
+++ html/Javascript/template.js
@@ -0,0 +1,23 @@
+const webLinks = {
+    "home": ["/", "Home"],
+    "projects": ["/projects", "Projects"],
+    "art": ["/art", "Art"],
+    "portfolio": ["/portfolio", "Portfolio"]
+    "contact": ["/contact", "Contact"],
+    "deepworld": ["/deepworld", "Deepworld"]
+};
+
+function loadWebLinks(element, socket, currentPage) {
+    for (var i = 0; i < Object.keys(webLinks).length; ++i) {
+        const currentWebLink = webLinks[Object.keys(webLinks)[i]];
+        const listElement = document.createElement("li");
+        listElement.classList.add("nav-item");
+        const linkElement = document.createElement("a");
+        linkElement.classList.add("nav-link");
+        if (Object.keys(webLinks)[i] != currentPage)
+            linkElement.href = socket + currentWebLink[0];
+        linkElement.innerText = currentWebLink[1];
+        listElement.appendChild(linkElement);
+        document.querySelector(element).appendChild(listElement);
+    }
+}
