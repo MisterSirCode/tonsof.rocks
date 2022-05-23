3c6fd0286119abf9de25a1684dd3e17df9c8949c
Sir Code <taylorschneider07@yahoo.com>
2022-05-22 21:46:32 -0400

    Major improvements

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,42 @@
 let alist = document.querySelectorAll('a');
 let frame = document.querySelector('.contentFrame');
+let cover = document.querySelector('.contentCover');
+let isLoaded = true;
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
 
 for (var el = 0; el < alist.length; el++) {
     let cel = alist[el];
     cel.addEventListener('click', (ev) => {
-        frame.src = `./pages/${cel.innerText}.html`;
+        loadPage(cel.innerText);
     });
+}
+
+frame.addEventListener('load', () => {
+    endCover();
+})
+
+function loadPage(name) {
+    startCover();
+    delay(250).then(() => {
+        frame.src = `./pages/${name}.html`;
+    });
+}
+
+function startCover() {
+    for (var el = 0; el < alist.length; el++) {
+        let cel = alist[el];
+        cel.classList.add('noclick');
+    }
+    cover.classList.add('visible');
+}
+
+function endCover() {
+    for (var el = 0; el < alist.length; el++) {
+        let cel = alist[el];
+        cel.classList.remove('noclick');
+    }
+    cover.classList.remove('visible');
 }
