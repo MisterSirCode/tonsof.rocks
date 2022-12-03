53a2b5c919d4db3b6e5e43224cc2563841b9b117
Sir Code <taylorschneider07@yahoo.com>
2022-12-03 10:46:21 -0500

    lotsa changes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,5 +2,19 @@ let copyright = 'Copyright © Tyler S (2022) - SV 0.0.1';
 
 function loadMods() {
     let body = document.body;
-
 }
+
+function makeLetterShadows() {
+    document.querySelectorAll(".letterSh").forEach((el) => {
+        let text = el.innerText;
+        el.innerText = '';
+        for (let i = 0; i < text.length; i++) {
+            let letter = document.createElement('a');
+            letter.className = 'sh';
+            letter.innerText = text[i];
+            el.appendChild(letter);
+        }
+    });
+}
+
+makeLetterShadows();
