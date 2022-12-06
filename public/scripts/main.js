066d3893b6323acad49e95fa2b01b0f6546260e9
Sir Code <taylorschneider07@yahoo.com>
2022-12-05 23:55:26 -0500

    update and fix the navigator

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,20 @@
 let copyright = 'Copyright © Tyler S (2023) - sv0.0.1';
-let pages = ['home', 'projects'];
+let pages = ['home', 'art', 'projects'];
+
+function drawLinkNavi() {
+    let parent = document.querySelector('.navCollection');
+    for (let p = 0; p < pages.length; p++) {
+        let el = document.createElement('div');
+        let innerEl = document.createElement('a');
+        el.className = 'navLink';
+        innerEl.className = 'navSubLink';
+        innerEl.innerText = pages[p][0].toUpperCase() + pages[p].substring(1);
+        el.appendChild(innerEl);
+        parent.appendChild(el);
+    }
+}
 
 window.onload = () => {
     document.querySelector('.copyright').innerText = copyright;
+    drawLinkNavi();
 };
