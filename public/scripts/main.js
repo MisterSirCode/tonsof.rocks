9ef752d2c38caf4d0a7ec5d57768103cb3a32cf6
Sir Code <taylorschneider07@yahoo.com>
2022-12-05 23:01:24 -0500

    Lots of changes to CSS and details

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,5 @@
 let copyright = 'Copyright © Tyler S (2023) - sv0.0.1';
+let pages = ['home', 'projects'];
 
 window.onload = () => {
     document.querySelector('.copyright').innerText = copyright;
