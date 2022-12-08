90822f64ee42eeda55dc3393b19170889aaf35f7
Sir Code <taylorschneider07@yahoo.com>
2022-12-08 00:50:31 -0500

    Adjustments + Mobile support

--- public/script/index.js
+++ public/script/index.js
@@ -2,12 +2,21 @@ const isMobile =
     navigator.userAgent.indexOf("Mobile") !== -1 || 
     navigator.userAgent.indexOf("iPhone") !== -1 || 
     navigator.userAgent.indexOf("Android") !== -1 || 
-    navigator.userAgent.indexOf("Windows Phone") !== -1 ||
-    window.innerHeight > window.innerWidth;
+    navigator.userAgent.indexOf("Windows Phone") !== -1;
 
 const $ = (s) => document.querySelector(s);
+const article = $('article');
+const collect = $('.flexboxCollection');
 
-if (isMobile) {
-    $('artiicle').classList.add('mobile');
-    $('.flexboxCollection').classList.add('mobile');
+function refresh() {
+    if (isMobile || window.innerHeight > window.innerWidth) {
+        article.classList.add('mobile');
+        collect.classList.add('mobile');
+    } else {
+        article.classList.remove('mobile');
+        collect.classList.remove('mobile');
+    }
 }
+
+window.addEventListener('DOMContentLoaded', refresh);
+window.addEventListener('resize', refresh);
