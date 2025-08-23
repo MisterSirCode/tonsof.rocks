8acd0f7e329137d933e2755b56958a308fff65f1
Taylor Schneider <taylorschneider07@yahoo.com>
2025-08-23 01:50:20 -0400

    More fixes

--- public/tools/complexConverter/js/index.js
+++ public/tools/complexConverter/js/index.js
@@ -1,18 +1,17 @@
 let el = document.querySelector('.startCode');
 el.addEventListener('input', (elm) => {
-    let strings = elm.target.value.split(' + i ');
-    strings[1] += " ";
-    strings[1] = strings[1].substring(1, strings[1].length - 1);
-    let fixed = [];
-    strings.forEach(string => {
-        fixed.push(
-            string.replace(/Im\[(\w)\]/g, '$1.y')
-            .replace(/Im\((\w)\)/g, '$1.y')
-            .replace(/Re\[(\w)\]/g, '$1.x')
-            .replace(/Re\((\w)\)/g, '$1.x')
-            .replace(/(\w\.\w)\^2/g, '($1 * $1)')
-            .replace(/(\w|\d)\s(\w|\d)/g, `$1 * $2`)
-            .replace(/(\d)\s/g, '$1.0 '));
-    });
-    document.querySelector('.endCode').textContent = `vec2(${fixed[0]}, ${fixed[1]})`;
+    let strings = elm.target.value
+        .replace(/ \+ (\d) i /g, 'i$1 * ')
+        .replace(/Im\[(\w)\]/g, '$1.y')
+        .replace(/Im\((\w)\)/g, '$1.y')
+        .replace(/Re\[(\w)\]/g, '$1.x')
+        .replace(/Re\((\w)\)/g, '$1.x')
+        .replace(/(\w\.\w)\^2/g, '$1 * $1')
+        .replace(/(\w|\d)\s(\w|\d)/g, `$1 * $2`)
+        .replace(/([0xy\)])\s(\w|\d|\()/g, '$1 * $2')
+        .replace(/arg\((\w)\)/g, 'atan($1.y, $1.x)')
+        .replace(/(\d)\s/g, '$1.0 ')
+    console.log(strings);
+    let fixed = strings.split('i');
+    document.querySelector('.endCode').textContent = `z = vec2(${fixed[0]},\n    ${fixed[1]});`;
 });
